2d2cb605e0f087f57edaac110ad3361b2e10f684
nataliebran <brangnatalie@gmail.com>
2019-08-02 10:37:11 -0400

    updated all of my  website files

---
+++ website_javascript.js
@@ -0,0 +1,11 @@
+function change_div_color () {
+  document.body.style.backgroundColor = "red";
+
+}
+window.alert("this is your #1 source for all things nat !");
+if (confirm("would you like to continue ?")) {
+  txt = "You pressed OK!";
+} else {
+  txt = "You pressed Cancel!";
+
+}
